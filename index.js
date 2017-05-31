32184e737fb409c77c1e5f521ecfc57c89191955
benedict-wellard <Benedict.Wellard@bjss.com>
2017-05-31 17:20:48 +0100

    Changing lambda return message to check updating

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ exports.handler = (event, context, callback) => {
     console.log('value1 =', event.key1);
     console.log('value2 =', event.key2);
     console.log('value3 =', event.key3);
-    callback(null, {"statusCode": 200, "body": "Success"});
+    callback(null, {"statusCode": 200, "body": "Success Again"});
     //callback('Something went wrong');
 };
