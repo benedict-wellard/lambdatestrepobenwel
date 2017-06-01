a227e62f72c2e833334e6ef7f92cab629a41b961
benedict-wellard <Benedict.Wellard@bjss.com>
2017-06-01 10:20:42 +0100

    Attempting dynamo put operation

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 'use strict';
 
-console.log('Loading function');
-
 exports.handler = (event, context, callback) => {
-    //console.log('Received event:', JSON.stringify(event, null, 2));
-    console.log('value1 =', event.key1);
-    console.log('value2 =', event.key2);
-    console.log('value3 =', event.key3);
+    var AWS = require("aws-sdk");
+
+    AWS.config.update({
+    region: "eu-west-1"
+    });
+
+    var docClient = new AWS.DynamoDB.DocumentClient();
+
+    var table = "Movies";
+
+    var year = 2015;
+    var title = "The Big New Movie";
+
+    var params = {
+        TableName:table,
+        Item:{
+            "year": year,
+            "title": title,
+            "info":{
+                "plot": "Nothing happens at all.",
+                "rating": 0
+            }
+        }
+    };
+
+    docClient.put(params, function(err, data) {
+        if (err) {
+            console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Added item:", JSON.stringify(data, null, 2));
+        }
+    });
+
     callback(null, {"statusCode": 200, "body": "Success Again"});
-    //callback('Something went wrong');
 };
