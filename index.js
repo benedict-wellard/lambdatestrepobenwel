4e1efc20cc4169c85983e13ac6a84bcb296f92f4
benedict-wellard <Benedict.Wellard@bjss.com>
2017-06-05 14:40:47 +0100

    Modifying dynamo table to accept request

--- index.js
+++ index.js
@@ -2,39 +2,37 @@
 
 exports.handler = (event, context, callback) => {
     var AWS = require("aws-sdk");
-    console.log("1");
+
     AWS.config.update({
-    endpoint: "https://dynamodb.eu-west-1.amazonaws.com",    
-    region: "eu-west-1"
+        endpoint: "https://dynamodb.eu-west-1.amazonaws.com",    
+        region: "eu-west-1"
     });
-    console.log("2");
+
     var dynamodb = new AWS.DynamoDB();
-    console.log("3");
     var table = "Movies";
-    console.log("4");
     var year = "2015";
     var title = "The Big New Movie";
+    var user = event.body.userID;
+    var goal = event.body.goal;
 
     var params = {
         TableName:table,
         Item:{
-            "year": 
-                {"S" : year},
-            "title":
-                {"S" : title},
-            "info":
-                {"S" : "Nothing happens at all."}
+            "user": 
+                {"S" : userID},
+            "goal":
+                {"S" : goal}
             }
         
     };
-    console.log("5");
+
     dynamodb.putItem(params, function(err, data) {
         if (err) {
             console.log("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+            callback(null, {"statusCode": 500, "body": "Failed"});
         } else {
             console.log("Added item:", JSON.stringify(data, null, 2));
+            callback(null, {"statusCode": 200, "body": "Success"});
         }
     });
-    console.log("6");
-    callback(null, {"statusCode": 200, "body": "Success Again"});
 };
